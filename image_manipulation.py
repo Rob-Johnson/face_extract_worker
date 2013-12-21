c95088d49fbc4addf07e27d5e0455327b0d52d06
Rob Johnson <rob@rob-johnson.me>
2013-12-21 12:37:12 +0000

    add resize function

---
+++ image_manipulation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+import sys
+import numpy
+import cv2
+
+## add the facerec module to the path - it's not available as a pip install yet
+sys.path.append('./facerec/py')
+
+import Image
+from facedet.detector import CascadedDetector
+
+
+def resize_image(image, dimensions=(10,10)):
+    """ Resize an image according to width and height values """
+    return image.resize(dimensions)
---
+++ test/image_manipulation_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import image_manipulation
+import unittest
+import Image
+
+class TestImageManipulation(unittest.TestCase):
+
+    def setUp(self):
+        self.image = Image.open('test/assets/rob.png')
+
+    def test_image_resize(self):
+        """ Test that the image resize function works as expecte """
+        dimensions = (20, 20)
+        resize_image = image_manipulation.resize_image(self.image, dimensions)
+        self.assertEqual(resize_image.size, dimensions)
+
+if __name__ == '__main__':
+    unittest.main()
