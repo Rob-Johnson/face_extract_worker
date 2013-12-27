bf1e8f77f86cd2503e677a9397edb88c9cbd2245
Rob Johnson <rob@rob-johnson.me>
2013-12-27 12:06:46 +0000

    add logging, get or create bucket

--- s3_tools.py
+++ s3_tools.py
@@ -3,6 +3,9 @@
 """ Upload files to amazon s3 """
 import boto
 import boto.s3
+import logging
+
+log = logging.getLogger("S3_TOOLS")
 
 def connect(aws_access_key_id=None, aws_secret_access_key=None):
     """ uses api_key and secret_key if available or
@@ -10,15 +13,22 @@ def connect(aws_access_key_id=None, aws_secret_access_key=None):
     if aws_access_key_id or aws_secret_access_key is None:
         return boto.connect_s3()
     else:
+        log.debug('Falling back to ENV variables $AWS_ACCESS_KEY && $AWS_SECRET_KEY')
         return boto.connect_s3(aws_access_key_id, aws_secret_access_key)
 
-def upload_file_to_bucket(bucket, key_name, source_file):
+def upload_file_to_bucket(bucket, key_name, source):
     """ uploads a file to a named S3 bucket """
+    log.debug('uploading to key_name %s', key_name)
+    log.debug('uploading content %s', source)
     key = boto.s3.key.Key(bucket)
     key.key = key_name
-    key.set_contents_from_filename(source_file)
+    key.set_contents_from_string(source)
 
-def get_bucket(conn, bucket_name):
-    """ returns a bucket from a name """
-    return conn.get_bucket(bucket_name)
-
+def get_or_create_bucket(conn, bucket_name):
+    """ get or create a bucket """
+    log.debug('finding bucket %s if it exists', bucket_name)
+    bucket = conn.lookup(bucket_name)
+    if bucket is None:
+        log.debug('bucket doesnt exist. creating bucket %s', bucket_name)
+        bucket = conn.create_bucket(bucket_name)
+    return bucket
