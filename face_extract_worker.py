ec41775de9a26df8c1a6b5f43c2eee8c8536c91d
Rob Johnson <rob@rob-johnson.me>
2013-12-27 12:07:29 +0000

    worker

---
+++ face_extract_worker.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import logging
+import pika
+import image_manipulation 
+import s3_tools
+import Image
+import json
+import requests
+from StringIO import StringIO
+
+class FaceExtractWorker:
+    def __init__(self):
+        rmq_connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host='localhost'))
+        self.rmq_channel = rmq_connection.channel()
+        self.rmq_channel.queue_declare(queue='user_images', durable=True)
+        self.s3_connection = s3_tools.connect()
+
+    def consume(self):
+        self.rmq_channel.basic_qos(prefetch_count=1)
+        self.rmq_channel.basic_consume(self.extract_upload,
+                queue='user_images')
+        self.rmq_channel.start_consuming()
+
+    def extract_upload(self,ch, method, properties, body):
+        """ JSON Payload should look like {'uid':1234, 'picture':'http://foo.bar', 'id':'facebookimageurl'} """
+        logger.info('Processing Record')
+        logger.debug('Message Body: %s',body)
+        body = json.loads(body)
+        image_url, user, photo_id = body['picture'], body['uid'], body['id']
+        normalized = image_manipulation.grayscale(
+                        image_manipulation.resize_image(get_image(image_url)))
+
+        [s3_tools.upload_file_to_bucket(user_bucket, '{}_{}_{}'.format(user,photo_id,index), extracted) for index, extracted in enumerate(roi)]
+        roi = image_manipulation.extract_faces(normalized)
+        logger.debug('found %d regions of interest', len(roi))
+        user_bucket = s3_tools.get_or_create_bucket(self.s3_connection, '{}'.format(user))
+        [s3_tools.upload_file_to_bucket(user_bucket, '{}_{}_{}'.format(user,photo_id,index), extracted) for index, extracted in enumerate(roi)]
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+
+def get_image(url):
+    response = requests.get(url)
+    return Image.open(StringIO(response.content))
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger("FACE_EXTRACT_WORKER")
+    worker = FaceExtractWorker()
+    worker.consume()
